608164f129cd1f7181cc819ee4d77b7ef86b9283
Wesley Rocha <wesleysr1997@gmail.com>
2023-10-10 08:20:19 -0400

    Add delta to leaderboards

--- main.ts
+++ main.ts
@@ -191,65 +191,63 @@ function onOpen() {
   ui.createMenu("Ações").addItem("Parse Tournament Sheet", "foo").addToUi();
 }
 
-function foo() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const seasonMatches: Match[] = [];
+function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+  const sheet = e.range.getSheet();
+  const name = sheet.getName();
+  if (name.startsWith("Rodada")) {
+    updateLeaderboards();
+  }
+}
 
-  ss.getSheets().forEach((sheet) => {
-    const name = sheet.getName();
-    if (isTournamentSheetName(name)) {
-      seasonMatches.push(...parseTournamentSheet(sheet));
-    }
-  });
-
-  let players: Record<string, Player> = {};
-
-  seasonMatches.forEach((match) => {
-    const winningTeam = match.teams.reduce((prev, curr) => {
-      return curr.roundsWon > prev.roundsWon ? curr : prev;
-    }, match.teams[0]);
-    const totalRounds = match.teams.reduce((prev, curr) => {
-      return prev + curr.roundsWon;
-    }, 0);
-
-    // Create players if they don't exist
-    match.teams.forEach((team) => {
-      team.players.forEach((nickname) => {
-        if (!(nickname in players)) {
-          players[nickname] = new Player(nickname);
-        }
-        const player = players[nickname];
-
-        if (!(match.tournamentId in player.tournaments)) {
-          player.tournaments[match.tournamentId] = {
-            wins: 0,
-            perfectWins: 0,
-            champion: false,
-            matchesWon: [],
-          };
-        }
-      });
-    });
-
-    // Apenas uma forma de verificar se foi twolala Em outras palavras, se a
-    // quantidade total de rounds da batalha foi igual a 2, então não houve
-    // terceiro round
-    const wasPerfectWin = totalRounds == 2;
-
-    winningTeam.players.forEach((nickname) => {
-      const player = players[nickname];
-      const tournament = player.tournaments[match.tournamentId];
-
-      tournament.wins++;
-      tournament.perfectWins += wasPerfectWin ? 1 : 0;
-      if (match.stage == Stage.Finals) {
-        tournament.champion = true;
+function updatePlayerDataWithMatchResult(
+  players: Record<string, Player>,
+  match: Match
+) {
+  // Create players if they don't exist
+  match.teams.forEach((team) => {
+    team.players.forEach((nickname) => {
+      if (!(nickname in players)) {
+        players[nickname] = new Player(nickname);
       }
-      tournament.matchesWon.push(match);
+      const player = players[nickname];
+
+      if (!(match.tournamentId in player.tournaments)) {
+        player.tournaments[match.tournamentId] = {
+          wins: 0,
+          perfectWins: 0,
+          champion: false,
+          matchesWon: [],
+        };
+      }
     });
   });
 
-  const leaderboard = Object.values(players)
+  const winningTeam = match.teams.reduce((prev, curr) => {
+    return curr.roundsWon > prev.roundsWon ? curr : prev;
+  }, match.teams[0]);
+  const totalRounds = match.teams.reduce((prev, curr) => {
+    return prev + curr.roundsWon;
+  }, 0);
+  // Apenas uma forma de verificar se foi twolala Em outras palavras, se a
+  // quantidade total de rounds da batalha foi igual a 2, então não houve
+  // terceiro round
+  const wasPerfectWin = totalRounds == 2;
+
+  winningTeam.players.forEach((nickname) => {
+    const player = players[nickname];
+    const tournament = player.tournaments[match.tournamentId];
+
+    tournament.wins++;
+    tournament.perfectWins += wasPerfectWin ? 1 : 0;
+    if (match.stage == Stage.Finals) {
+      tournament.champion = true;
+    }
+    tournament.matchesWon.push(match);
+  });
+}
+
+function generateLeaderboards(players: Record<string, Player>): Player[] {
+  return Object.values(players)
     .filter((player) => player.getScore() > 0)
     .sort(comparePlayers)
     .map((player, index, array) => {
@@ -264,6 +262,40 @@ function foo() {
 
       return player;
     });
+}
+
+function foo() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const seasonMatches: Match[] = [];
+
+  ss.getSheets().forEach((sheet) => {
+    const name = sheet.getName();
+    if (isTournamentSheetName(name)) {
+      seasonMatches.push(...parseTournamentSheet(sheet));
+    }
+  });
+
+  let players: Record<string, Player> = {};
+  seasonMatches.forEach((match) => {
+    updatePlayerDataWithMatchResult(players, match);
+  });
+
+  const lastTournament = seasonMatches.reduce(
+    (prev, curr) => (curr.tournamentId > prev.tournamentId ? curr : prev),
+    seasonMatches[0]
+  ).tournamentId;
+  const previousMatches = seasonMatches.filter(
+    (match) => match.tournamentId !== lastTournament
+  );
+  let previousPlayerData: Record<string, Player> = {};
+  previousMatches.forEach((match) => {
+    updatePlayerDataWithMatchResult(previousPlayerData, match);
+  });
+
+  const leaderboard = generateLeaderboards(players);
+  generateLeaderboards(previousPlayerData).forEach((player) => {
+    previousPlayerData[player.nickname].position = player.position;
+  });
 
   const sheet = ss.getSheetByName("Placar");
   if (!sheet) {
@@ -274,12 +306,52 @@ function foo() {
 
   leaderboard.map((player, index) => {
     const range = sheet.getRange(index + 3, 1, 1, 5);
-    range.setValues([
+
+    const previous = previousPlayerData[player.nickname];
+    if (!previous) {
+      return range.setValues([
+        [
+          player.position,
+          player.nickname,
+          player.getScore(),
+          player.getPerfectWins(),
+          player.getParticipations(),
+        ],
+      ]);
+    }
+
+    let score = player.getScore().toString();
+    const scoreDelta = player.getScore() - previous.getScore();
+    if (scoreDelta) {
+      score += ` +${scoreDelta}`;
+    }
+
+    let perfectWins = player.getPerfectWins().toString();
+    const perfectWinsDelta =
+      player.getPerfectWins() - previous.getPerfectWins();
+    if (perfectWinsDelta) {
+      perfectWins += ` +${perfectWinsDelta}`;
+    }
+
+    let position = "";
+    if (
+      !player.position ||
+      !previous.position ||
+      player.position === previous.position
+    ) {
+    } else if (player.position > previous.position) {
+      position = "⬇ ️";
+    } else if (player.position < previous.position) {
+      position = "⬆️ ";
+    }
+    position += player.position?.toString();
+
+    return range.setValues([
       [
-        player.position,
+        position,
         player.nickname,
-        player.getScore(),
-        player.getPerfectWins(),
+        score,
+        perfectWins,
         player.getParticipations(),
       ],
     ]);
