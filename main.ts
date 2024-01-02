6c8f9069d49f365a009bbfa24999a6cde6ed7578
Wesley Rocha <wesleysr1997@gmail.com>
2024-01-02 01:38:39 -0400

    Remove unused code + stuff

--- main.ts
+++ main.ts
@@ -1,237 +1,361 @@
-enum Delta {
-  NONE = "",
-  UP = "▲",
-  DOWN = "▼",
-}
-
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
-  ui.createMenu("Ações")
-    .addItem("Atualizar ranking", "updateLeaderboard")
-    .addItem("Atualizar players", "updatePlayerSheets")
-    .addToUi();
+  ui.createMenu("Ações").addItem("Executar", "execute").addToUi();
 }
 
-function updatePlayerDataWithMatchResult(
-  players: Record<string, Player>,
-  match: Match
+interface Team {
+  players: string[]; // nickname
+  roundsWon: number;
+}
+
+interface Match {
+  raw: string;
+  host: string;
+  date: string;
+  stage: Stage;
+  teams: Team[];
+}
+
+interface Player {
+  nickname: string;
+  matches: Match[];
+}
+
+enum Stage {
+  Unknown = "Desconhecido",
+  EightFinals = "Oitavas de Final",
+  QuarterFinals = "Quartas de Final",
+  SemiFinals = "Semifinal",
+  Finals = "Final",
+}
+
+interface Tournament {
+  date: string;
+  host: string;
+  matches: Match[];
+}
+
+function getTeamsFromMatchResults(data: string): Team[] {
+  // Scoreless
+  // E.g.: Blink e Killer* x Kenny e Kennyzin
+  //       Onec x Jhones*
+  if (data.includes("*")) {
+    return data.split(" x ").map((team) => ({
+      players: team.replace("*", "").split(" e "),
+      roundsWon: team.includes("*") ? 1 : 0,
+    }));
+  }
+
+  // With score, no double-three
+  // E.g.: RK 2 x 0 Big Xang
+  //       Eva e Isa 2 x 1 Mont e Onec
+  if (/ \d x \d /.test(data)) {
+    const [full, roundsWon1, roundsWon2] = / (\d) x (\d) /.exec(data) || [];
+    // Use the extracted groups in your code
+    const roundsResult = [roundsWon1, roundsWon2];
+
+    return data.split(full!).map((team, i) => ({
+      players: team.split(" e "),
+      roundsWon: parseInt(roundsResult[i]),
+    }));
+  }
+
+  // With score, double-three
+  // E.g.: Berg 2 x Barb 0 x Sharp 1
+
+  return [];
+}
+
+function toStage(rawStage: string): Stage {
+  rawStage = rawStage.toLocaleLowerCase();
+  if (rawStage == "primeira fase") {
+    return Stage.EightFinals;
+  } else if (rawStage == "segunda fase") {
+    return Stage.QuarterFinals;
+  } else if (rawStage == "semifinal" || rawStage == "semi final") {
+    return Stage.SemiFinals;
+  } else if (rawStage == "final") {
+    return Stage.Finals;
+  } else {
+    return Stage.Unknown;
+  }
+}
+
+function getMatches(
+  data: string[][],
+  { host, date }: { host: string; date: string }
 ) {
-  // Create players if they don't exist
-  match.teams.forEach((team) => {
-    team.players.forEach((nickname) => {
-      if (!(nickname in players)) {
-        players[nickname] = new Player(nickname);
-      }
-      const player = players[nickname];
+  let matches: Match[] = [];
 
-      if (!(match.tournamentId in player.tournaments)) {
-        player.tournaments[match.tournamentId] = {
-          wins: 0,
-          perfectWins: 0,
-          champion: false,
-          matches: [],
-          matchesWon: [],
-        };
-      }
-
-      const tournament = player.tournaments[match.tournamentId];
-      tournament.matches.push(match);
+  data.forEach((row, i) => {
+    const stage = row[0] ? toStage(row[0]) : matches[i - 1].stage;
+    matches.push({
+      raw: row[1],
+      date,
+      host,
+      stage,
+      teams: getTeamsFromMatchResults(row[1]),
     });
   });
 
-  const winningTeam = match.teams.reduce((prev, curr) => {
+  if (!matches.map((match) => match.stage).includes(Stage.QuarterFinals)) {
+    matches = matches.map((match) => ({
+      ...match,
+      stage:
+        match.stage == Stage.EightFinals ? Stage.QuarterFinals : match.stage,
+    }));
+  }
+
+  return matches;
+}
+
+// Exemplo de uma edição (tournament). Atualmente o código só considera a
+// primeira organização e ignora a edição e os modos.
+// E.g.:
+//       Data          | 27/08/2023 <- Formato de Data no Google Sheets
+//       Organização   | Batalha das Minas     | Batalha da La Prata |
+//       Edição        | Especial de Halloween |                     |
+//                     |                       |                     |
+//       Primeira Fase | Barb* x Giza          |                     |
+//                     | Pedrina* x Dark       |                     |
+//                     | RK* x Atna            |                     |
+//                     | Eva* x Jogadora <- Edição com apenas 3 fases,
+//                                          mas geralmente é 4
+//       Semifinal     | Pedrina* x Barb       |                     |
+//                     | RK* x Eva             |                     |
+//       Final         | RK* x Pedrina         |                     |
+//       Campeão       | RK                    |                     |
+function getTournament(data: string[][]): Tournament {
+  const date = data[0][1];
+  const host = data[1][1];
+  const matches = getMatches(data.slice(4, -1), { host, date });
+
+  return {
+    date,
+    host,
+    matches,
+  };
+}
+
+function validateTournament(data: string[][]) {
+  const errors: string[] = [];
+  if (data[0][0] !== "Data") errors.push("Linha 1 deveria ter campo 'Data'");
+  if (!data[0][1]) errors.push("Informe a data");
+  if (data[1][0] !== "Organização")
+    errors.push("Linha 2 deveria ter campo 'Organização'");
+  if (!data[1][1]) errors.push("Informe a organização");
+  if (data[2][0] !== "Edição")
+    errors.push("Linha 3 deveria ter campo 'Edição'");
+  if (data[3][0]) errors.push("Linha 4 deveria estar vazia");
+
+  if (
+    toStage(data[4][0]) !== Stage.EightFinals &&
+    toStage(data[4][0]) !== Stage.SemiFinals
+  ) {
+    errors.push("Linha 5 deveria iniciar com o resultado das batalhas");
+  }
+
+  if (errors.length) {
+    throw new Error(errors.join(" | "));
+  }
+}
+
+function getTournaments(data: string[][]): Tournament[] {
+  const tournaments: Tournament[] = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] !== "Data") {
+      continue;
+    }
+
+    let k = 0;
+    // Search until end of current tournament or end of range
+    // End of range can mean two things:
+    // - User selected the begining of another tournament by mistake
+    // - The "Campeao" string is missing for this tournament
+    while (i + k < data.length && data[i + k][0] !== "Campeão") {
+      k++;
+    }
+
+    // Skips on end of range
+    if (i + k === data.length) {
+      continue;
+    }
+
+    const slicedData = data.slice(i, i + k + 1);
+
+    validateTournament(slicedData);
+
+    const tournament = getTournament(slicedData);
+    tournaments.push(tournament);
+  }
+  return tournaments;
+}
+
+function getWinners(match: Match): string[] {
+  return match.teams.reduce((prev, curr) => {
+    // Assuming draws will never happen...
     return curr.roundsWon > prev.roundsWon ? curr : prev;
-  }, match.teams[0]);
-  const totalRounds = match.teams.reduce((prev, curr) => {
-    return prev + curr.roundsWon;
-  }, 0);
-  // Apenas uma forma de verificar se foi twolala Em outras palavras, se a
-  // quantidade total de rounds da batalha foi igual a 2, então não houve
-  // terceiro round
-  const wasPerfectWin = totalRounds == 2;
+  }, match.teams[0]).players;
+}
 
-  winningTeam.players.forEach((nickname) => {
-    const player = players[nickname];
-    const tournament = player.tournaments[match.tournamentId];
-
-    tournament.wins++;
-    tournament.perfectWins += wasPerfectWin ? 1 : 0;
-    if (match.stage == Stage.Finals) {
-      tournament.champion = true;
+// Many teams can lose at the same time. For the sake of simplicity, this
+// functions returns a single team of all losers
+function getLosers(match: Match): string[] {
+  const losers: string[] = [];
+  const maxRoundsWon = Math.max(...match.teams.map((team) => team.roundsWon));
+  match.teams.forEach((team) => {
+    if (team.roundsWon < maxRoundsWon) {
+      losers.push(...team.players);
     }
-    tournament.matchesWon.push(match);
   });
+  return losers;
 }
 
-function generateLeaderboards(players: Record<string, Player>): Player[] {
-  return (
-    Object.values(players)
-      // Desative o filtro para verificar se tá tudo certo
-      .filter((player) => player.getScore() > 0)
-      .sort(comparePlayers)
-      .map((player, index, array) => {
-        if (index == 0) {
-          player.position = 1;
-          return player;
-        }
-
-        const drawsWithLast = comparePlayers(player, array[index - 1]) === 0;
-        if (drawsWithLast) {
-          player.position = array[index - 1].position;
-        } else {
-          player.position = index + 1;
-        }
-        return player;
-      })
-  );
-}
-
-function readPlayerData(): Record<string, Player> {
+function execute() {
+  const ui = SpreadsheetApp.getUi();
   const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const seasonMatches: Match[] = [];
+  const sheet = ss.getActiveSheet();
 
-  ss.getSheets().forEach((sheet) => {
-    const name = sheet.getName();
-    if (isTournamentSheetName(name)) {
-      seasonMatches.push(...parseTournamentSheet(sheet));
-    }
-  });
+  // First, consider user selected range
+  let data = sheet.getActiveRange()?.getValues() as string[][];
+  let tournaments = getTournaments(data);
 
-  let players: Record<string, Player> = {};
-
-  seasonMatches.forEach((match) => {
-    updatePlayerDataWithMatchResult(players, match);
-  });
-
-  return players;
-}
-
-function updateLeaderboard() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const seasonMatches: Match[] = [];
-
-  ss.getSheets().forEach((sheet) => {
-    const name = sheet.getName();
-    if (isTournamentSheetName(name)) {
-      seasonMatches.push(...parseTournamentSheet(sheet));
-    }
-  });
-
-  let players: Record<string, Player> = {};
-
-  seasonMatches.forEach((match) => {
-    updatePlayerDataWithMatchResult(players, match);
-  });
-
-  const lastTournament = seasonMatches.reduce(
-    (prev, curr) => (curr.tournamentId > prev.tournamentId ? curr : prev),
-    seasonMatches[0]
-  ).tournamentId;
-  const previousMatches = seasonMatches.filter(
-    (match) => match.tournamentId !== lastTournament
-  );
-  // Equivalente à seasonMatches, mas indo apenas até a penúltima rodada
-  let previousPlayerData: Record<string, Player> = {};
-  previousMatches.forEach((match) => {
-    updatePlayerDataWithMatchResult(previousPlayerData, match);
-  });
-
-  const leaderboard = generateLeaderboards(players);
-  generateLeaderboards(previousPlayerData).forEach((player) => {
-    previousPlayerData[player.nickname].position = player.position;
-  });
-
-  const sheet = ss.getSheetByName("Placar");
-  if (!sheet) {
-    return;
+  if (tournaments.length == 0) {
+    data = sheet.getDataRange().getValues();
+    tournaments = getTournaments(data);
   }
 
-  sheet.getRange(3, 1, 100, 7).clearContent();
+  const matches = tournaments.flatMap((tournament) => tournament.matches);
 
-  leaderboard.map((player, index) => {
-    const range = sheet.getRange(index + 3, 1, 1, 7);
+  const matchSheet = ss.getSheetByName("Batalhas");
+  if (matchSheet) {
+    matchSheet.clear();
 
-    const previous = previousPlayerData[player.nickname];
-    if (!previous) {
-      const score = player.getScore() ? `+${player.getScore()}` : "";
-      return range.setValues([
-        [
-          player.position,
-          player.nickname,
-          score ? Delta.UP : Delta.NONE,
-          score,
-          player.getScore(),
-          player.getPerfectWins(),
-          player.getParticipations(),
-        ],
-      ]);
-    }
+    const headers = [
+      "Data",
+      "Organização",
+      "Fase",
+      "Batalha",
+      "Vencedor(es)",
+      "Perdedor(es)",
+    ];
+    matchSheet.getRange(1, 1, 1, headers.length).setValues([headers]);
 
-    const scoreDelta = player.getScore() - previous.getScore();
-    let tournamentScore = "";
-    if (scoreDelta < 0) {
-      tournamentScore = scoreDelta.toString();
-    } else if (scoreDelta > 0) {
-      tournamentScore = `+${scoreDelta}`;
-    }
-
-    let delta: string = Delta.NONE;
-    if (
-      !player.position ||
-      !previous.position ||
-      player.position == previous.position
-    ) {
-      delta = Delta.NONE;
-    } else if (player.position > previous.position) {
-      delta = `${player.position - previous.position} ${Delta.DOWN}`;
-    } else if (player.position < previous.position) {
-      delta = `${previous.position - player.position} ${Delta.UP}`;
-    }
-
-    return range.setValues([
-      [
-        player.position,
-        player.nickname,
-        delta,
-        tournamentScore,
-        player.getScore(),
-        player.getPerfectWins(),
-        player.getParticipations(),
-      ],
+    const matchTable = matches.map((match) => [
+      match.date,
+      match.host,
+      match.stage,
+      match.raw,
+      getWinners(match).join(" e "),
+      getLosers(match).join(" e "),
     ]);
-  });
-}
 
-// not ready yet
-function updatePlayerSheets() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const players = generateLeaderboards(readPlayerData());
+    if (headers.length !== matchTable[0].length) {
+      throw new Error(`Headers length does not match matchTable number of columns on sheet "MCs".
+        headers.length => ${headers.length}
+        matchTable[0].length => ${matchTable[0].length}`);
+    }
 
-  let sheet = ss.getSheetByName("MCs");
-  if (!sheet) {
-    sheet = ss.insertSheet("MCs");
+    const range = matchSheet.getRange(2, 1, matches.length, headers.length);
+    range.setValues(matchTable);
   }
-  sheet.clearContents();
 
-  const totalTournaments = ss.getSheets().reduce((prev, curr) => {
-    return prev + +isTournamentSheetName(curr.getName());
-  }, 0);
+  interface PlayerData extends Player {
+    // totalMatches: number | equivalent to totalMatches => matches.length
+    totalWins: number;
+    soloWins: number;
+  }
 
-  players.forEach((player, index) => {
-    const values = [
-      [player.nickname, ""],
-      [`RANK #${player.position}`, `${player.getScore()} pontos`],
-      ["Títulos", player.getTournamentsWon()],
-      ["Vitórias", player.getWins()],
-      ["Derrotas", player.getDefeats()],
-      ["Twolalas", player.getPerfectWins()],
-      // TODO: Fix participation bug, repeating in random places
-      ["Participações", `${player.getParticipations()}/${totalTournaments}`],
-      ["", ""],
+  const players: Record<string, PlayerData> = {};
+
+  matches.forEach((match) => {
+    // Create players that didn't exist before
+    match.teams.forEach((team) => {
+      team.players.forEach((nickname) => {
+        if (!(nickname in players)) {
+          players[nickname] = {
+            nickname,
+            matches: [],
+            totalWins: 0,
+            soloWins: 0,
+          };
+        }
+
+        players[nickname].matches.push(match);
+      });
+    });
+
+    const winners = getWinners(match);
+
+    winners.forEach((winner) => {
+      players[winner].totalWins++;
+      if (winners.length === 1) {
+        players[winner].soloWins++;
+      }
+    });
+  });
+
+  const playerSheet = ss.getSheetByName("MCs");
+  if (playerSheet) {
+    playerSheet.clear();
+
+    const headers = [
+      "Vulgo",
+      "Batalhas",
+      "Vitórias (total)",
+      "Vitórias (solo)",
     ];
 
-    sheet!
-      .getRange(index * values.length + 1, 1, values.length, 2)
-      .setValues(values);
-  });
+    playerSheet.getRange(1, 1, 1, headers.length).setValues([headers]);
+
+    const playerTable = Object.values(players).map((player) => [
+      player.nickname,
+      player.matches.length,
+      player.totalWins,
+      player.soloWins,
+    ]);
+
+    if (headers.length !== playerTable[0].length) {
+      throw new Error(`Headers length does not match playerTable number of columns on sheet "MCs".
+      headers.length => ${headers.length}
+      playerTable[0].length => ${playerTable[0].length}`);
+    }
+
+    const range = playerSheet.getRange(
+      2,
+      1,
+      playerTable.length,
+      headers.length
+    );
+    range.setValues(playerTable);
+  }
+
+  // ui.alert(matches.map(printMatch).join("\n"));
+
+  // ui.alert(
+  //   j(
+  //     tournaments.map((tournament) => ({
+  //       date: Utilities.formatDate(
+  //         new Date(tournament.date),
+  //         "GMT-0400",
+  //         "EEEE, dd/MM/yyyy"
+  //       ),
+  //       host: tournament.host,
+  //       firstMatch: printMatch(tournament.matches[0]),
+  //       lastMatch: printMatch(
+  //         tournament.matches[tournament.matches.length - 1]
+  //       ),
+  //     }))
+  //   )
+  // );
+}
+
+function j(d: any) {
+  return JSON.stringify(d, null, "--");
+}
+
+function printMatch(match: Match): string {
+  return match.teams
+    .map((team) => `${team.players.join(" e ")} (${team.roundsWon})`)
+    .join(" x ");
 }
